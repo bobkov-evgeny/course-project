015aeb922bad6bf356030344f659b92cfd5e0358
Evgenii <freeedayy@gmail.com>
2021-10-20 22:30:00 +0300

    forms

--- src/components/layouts/loginPage.jsx
+++ src/components/layouts/loginPage.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LoginPage = () => {
-    return <h1>login page</h1>;
+    const [data, setData] = useState({ email: "", password: "" });
+
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({ ...prevState, [target.name]: target.value }));
+    };
+
+    return (
+        <form action="">
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email" value={data.email} onChange={handleChange} name="email"/>
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" value={data.password} onChange={handleChange} name="password"/>
+            </div>
+        </form>
+    );
 };
 
 export default LoginPage;
